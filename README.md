33a93a630aa9b4e00f49da155e500c0138703766
iamchoking <iamchoking247@gmail.com>
2024-11-26 19:31:09 +0900

    modified tester and cleaned up data

--- README.md
+++ README.md
@@ -14,20 +14,20 @@ python setup develop --CMAKE_PREFIX_PATH ~/raisim_ws/raisimLib/raisim/linux
 ```
 2. run runner.py of the task (for anymal example): 
 ```sh
-python raisimGymTorch/env/envs/rsg_anymal/runner.py
+python raisimGymTorch/env/envs/cs672_pogo_example/runner.py
 ```
 
 ### Test policy
 1. Compile raisimgym (see **Run** above)
 2. run tester.py of the task with policy (for anymal example): 
 ```sh
-python raisimGymTorch/env/envs/rsg_anymal/tester.py --weight data/roughTerrain/FOLDER_NAME/full_XXX.pt
+python raisimGymTorch/env/envs/cs672_pogo_example/tester.py --weight raisimGymTorch/data/pogo_example/XXX/full_YYYY.pt
 ```
 
 ### Retrain policy
 1. run runner.py of the task with policy (for anymal example): 
 ```sh
-python raisimGymTorch/env/envs/rsg_anymal/runner.py --mode retrain --weight data/roughTerrain/FOLDER_NAME/full_XXX.pt
+python raisimGymTorch/env/envs/cs672_pogo_example/runner.py --mode retrain --weight raisimGymTorch/data/pogo_example/XXX/full_YYYY.pt
 ```
 
 ### Debugging
@@ -37,4 +37,4 @@ python setup develop --Debug
 ```
 This compiles <YOUR_APP_NAME>_debug_app
 
-2. Run it with Valgrind. I strongly recommend using Clion.
+2. Run it with Valgrind. (Clion is strongly recommended)
--- raisimGymTorch/env/envs/cs672_pogo_example/tester.py
+++ raisimGymTorch/env/envs/cs672_pogo_example/tester.py
@@ -1,5 +1,5 @@
 from ruamel.yaml import YAML, dump, RoundTripDumper
-from raisimGymTorch.env.bin import rsg_anymal
+from raisimGymTorch.env.bin import cs672_pogo_example
 from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecEnv as VecEnv
 import raisimGymTorch.algo.ppo.module as ppo_module
 import os
@@ -12,11 +12,12 @@ import argparse
 # configuration
 parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--weight', help='trained weight path', type=str, default='')
+parser.add_argument('-t', '--time'  , help='simulation total time', type=float, default=-1)
 args = parser.parse_args()
 
 # directories
 task_path = os.path.dirname(os.path.realpath(__file__))
-home_path = task_path + "/../../../../.."
+home_path = task_path + "/../../../.."
 
 # config
 cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
@@ -24,19 +25,27 @@ cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
 # create environment from the configuration file
 cfg['environment']['num_envs'] = 1
 
-env = VecEnv(rsg_anymal.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
+sim_time = args.time
+
+if sim_time > 0.0:
+    cfg['environment']['max_time'] = sim_time
+
+print(cfg['environment'])
+
+env = VecEnv(cs672_pogo_example.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
 
 # shortcuts
 ob_dim = env.num_obs
 act_dim = env.num_acts
 
 weight_path = args.weight
-iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
-weight_dir = weight_path.rsplit('/', 1)[0] + '/'
 
 if weight_path == "":
     print("Can't find trained weight, please provide a trained weight with --weight switch\n")
 else:
+    iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
+    weight_dir = weight_path.rsplit('/', 1)[0] + '/'
+
     print("Loaded weight from {}\n".format(weight_path))
     start = time.time()
     env.reset()
